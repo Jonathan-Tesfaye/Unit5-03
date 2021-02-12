3d4c435de6396369f14f25eef5e922e4f971749d
JonathanTesfaye <jonathan.tesfaye@ocsbstudent.ca>
2021-02-12 13:49:32 +0000

    code changes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.getElementById("button").addEventListener("click",buttonOn)
+document.getElementById('button').addEventListener('click', buttonOn)
 
-function buttonOn() {
-  document.getElementById("button-function").innerHTML = "See, I told you the text would change."
-  document.getElementById("cool-car").src = "pug.jfif"
-  document.getElementById("cool-car").alt = "A pug"
-  document.getElementById("background-color").style.backgroundColor = "grey"
-
+function buttonOn () {
+  document.getElementById('button-function').innerHTML = 'See, I told you the text would change.'
+  document.getElementById('cool-car').src = 'pug.jfif'
+  document.getElementById('cool-car').alt = 'A pug'
+  document.getElementById('background-color').style.backgroundColor = 'grey'
 }
